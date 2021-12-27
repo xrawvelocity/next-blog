4ee0d2a20a38164c419a4b0703e461da3435e5f9
xrawvelocity <vmfs99@gmail.com>
2021-12-27 15:21:27 -0500

    done with tutorial

---
+++ lib/firebase.ts
@@ -0,0 +1,57 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/storage";
+
+const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGINGSENDERID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
+};
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+export const auth = firebase.auth();
+export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+
+export const firestore = firebase.firestore();
+
+// Storage exports
+export const storage = firebase.storage();
+export const STATE_CHANGED = firebase.storage.TaskEvent.STATE_CHANGED;
+
+/**`
+ * Gets a users/{uid} document with username
+ * @param  {string} username
+ */
+export async function getUserWithUsername(username) {
+    const usersRef = firestore.collection("users");
+    const query = usersRef.where("username", "==", username).limit(1);
+    const userDoc = (await query.get()).docs[0];
+    return userDoc;
+}
+
+/**`
+ * Converts a firestore document to JSON
+ * @param  {DocumentSnapshot} doc
+ */
+export function postToJSON(doc) {
+    const data = doc.data();
+    return {
+        ...data,
+        // Gotcha! firestore timestamp NOT serializable to JSON. Must convert to milliseconds
+        createdAt: data.createdAt.toMillis(),
+        updatedAt: data.updatedAt.toMillis(),
+    };
+}
+
+export const fromMillis = firebase.firestore.Timestamp.fromMillis;
+
+export const serverTimestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export const increment = firebase.firestore.FieldValue.increment;
